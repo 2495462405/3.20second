609d995cd5845d9793b8a2b5229454dfaeb7a4c4
xuyishen <xzj2495462405@outlook.com>
2020-03-20 15:22:25 +0800

    2020.3.20第二题

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -38,42 +38,20 @@ int main()
 
     cout << " number is " << nComp-1 << endl;
 
-    resultMat = cv::Mat::zeros(srcMat.size(), CV_8UC3);
-    std::vector<cv::Vec3b> colors(nComp);
-
-    colors[0] = cv::Vec3b(0, 0, 0);
-
-    for (int n = 1; n < nComp; n++)
-    {
-        colors[n] = cv::Vec3b( 255,  255,  255);
-    }
-    for (int y = 0; y < srcMat.rows; y++)
-    {
-        for (int x = 0; x < srcMat.cols; x++)
-        {
-            int label = labelMat.at<int>(y, x);
-            CV_Assert(0 <= label && label <= nComp);
-            resultMat.at<cv::Vec3b>(y, x) = colors[label];
-        }
-    }
 
     for (int i = 1; i < nComp; i++)
     {
-        char num[10];
-        sprintf(num, "%d", i);
-
         Rect bndbox;
         bndbox.x=statsMat.at<int>(i,0);
         bndbox.y=statsMat.at<int>(i,1);
         bndbox.width=statsMat.at<int>(i,2);
         bndbox.height=statsMat.at<int>(i,3);
-        rectangle(resultMat,bndbox,CV_RGB(255,255,255),1,8,0);
+        rectangle(binaryMat,bndbox,CV_RGB(255,255,255),1,8,0);
 
     }
 
 
     imshow("binaryMat", binaryMat);
-    imshow("results", resultMat);
     imshow("frame", srcMat);
     waitKey(0);
     return 0;
